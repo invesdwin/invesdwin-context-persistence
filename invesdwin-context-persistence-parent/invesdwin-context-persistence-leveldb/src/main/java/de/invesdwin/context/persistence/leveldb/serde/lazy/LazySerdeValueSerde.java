cf7bada139e359d429101011c536087ae244daf0
subes <gsubes@gmail.com>
2016-09-01 03:09:44 +0200

    lazySerde with chooseable threadlocal strategy

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-leveldb/src/main/java/de/invesdwin/context/persistence/leveldb/serde/lazy/LazySerdeValueSerde.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-leveldb/src/main/java/de/invesdwin/context/persistence/leveldb/serde/lazy/LazySerdeValueSerde.java
@@ -2,20 +2,23 @@ package de.invesdwin.context.persistence.leveldb.serde.lazy;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import de.invesdwin.context.persistence.leveldb.serde.lazy.factory.ILazySerdeValueFactory;
 import ezdb.serde.Serde;
 
 @NotThreadSafe
 public class LazySerdeValueSerde<E> implements Serde<ILazySerdeValue<E>> {
 
     private final Serde<E> delegate;
+    private final ILazySerdeValueFactory factory;
 
-    public LazySerdeValueSerde(final Serde<E> delegate) {
+    public LazySerdeValueSerde(final Serde<E> delegate, final ILazySerdeValueFactory factory) {
         this.delegate = delegate;
+        this.factory = factory;
     }
 
     @Override
     public ILazySerdeValue<E> fromBytes(final byte[] bytes) {
-        return new LazySerdeValue<E>(delegate, bytes);
+        return factory.fromBytes(delegate, bytes);
     }
 
     @Override
