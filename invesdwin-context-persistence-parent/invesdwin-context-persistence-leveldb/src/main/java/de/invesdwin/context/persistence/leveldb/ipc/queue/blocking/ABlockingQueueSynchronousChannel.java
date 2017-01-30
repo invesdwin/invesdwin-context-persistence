22eb93f1b26b3ee769d84e3e689f689be143c542
subes <gsubes@gmail.com>
2017-01-30 01:18:03 +0100

    use xoroshiro

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-leveldb/src/main/java/de/invesdwin/context/persistence/leveldb/ipc/queue/blocking/ABlockingQueueSynchronousChannel.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-leveldb/src/main/java/de/invesdwin/context/persistence/leveldb/ipc/queue/blocking/ABlockingQueueSynchronousChannel.java
@@ -13,7 +13,7 @@ import de.invesdwin.context.persistence.leveldb.ipc.queue.QueueSynchronousWriter
 import de.invesdwin.util.bean.tuple.Pair;
 import de.invesdwin.util.concurrent.Executors;
 import de.invesdwin.util.concurrent.WrappedExecutorService;
-import de.invesdwin.util.math.RandomGeneratorAdapter;
+import de.invesdwin.util.math.random.RandomGenerators;
 
 @NotThreadSafe
 public abstract class ABlockingQueueSynchronousChannel implements ISynchronousChannel {
@@ -44,7 +44,7 @@ public abstract class ABlockingQueueSynchronousChannel implements ISynchronousCh
             @Override
             public void run() {
                 //randomize sleeps to increase chance of meeting each other
-                final RandomGenerator random = RandomGeneratorAdapter.currentThreadLocalRandom();
+                final RandomGenerator random = RandomGenerators.currentThreadLocalRandom();
                 try {
                     boolean closedMessageSent = false;
                     boolean closedMessageReceived = false;
