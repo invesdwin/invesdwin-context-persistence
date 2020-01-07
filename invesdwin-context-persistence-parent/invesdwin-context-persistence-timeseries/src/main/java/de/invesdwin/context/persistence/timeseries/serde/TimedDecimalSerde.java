ea6cef65eef2f9fa1136b199e20e524fb0f3a9d3
subes <gsubes@gmail.com>
2020-01-07 16:53:39 +0100

    persisting expression if needed

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseries/src/main/java/de/invesdwin/context/persistence/timeseries/serde/TimedDecimalSerde.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseries/src/main/java/de/invesdwin/context/persistence/timeseries/serde/TimedDecimalSerde.java
@@ -4,7 +4,7 @@ import java.nio.ByteBuffer;
 
 import javax.annotation.concurrent.Immutable;
 
-import de.invesdwin.util.math.decimal.Decimal;
+import de.invesdwin.util.assertions.Assertions;
 import de.invesdwin.util.math.decimal.TimedDecimal;
 import de.invesdwin.util.time.fdate.FDate;
 import de.invesdwin.util.time.fdate.FDates;
@@ -16,15 +16,22 @@ public class TimedDecimalSerde implements Serde<TimedDecimal> {
     public static final TimedDecimalSerde GET = new TimedDecimalSerde();
     public static final Integer FIXED_LENGTH = 8 + 8;
 
+    public static final FixedLengthBufferingIteratorDelegateSerde<TimedDecimal> GET_LIST = new FixedLengthBufferingIteratorDelegateSerde<TimedDecimal>(
+            GET, FIXED_LENGTH);
+
+    static {
+        Assertions.assertThat(GET.fromBytes(GET.toBytes(TimedDecimal.DUMMY))).isEqualTo(TimedDecimal.DUMMY);
+    }
+
     public TimedDecimalSerde() {}
 
     @Override
     public TimedDecimal fromBytes(final byte[] bytes) {
         final ByteBuffer buffer = ByteBuffer.wrap(bytes);
         final FDate time = FDates.extractFDate(buffer);
-        final Decimal percent = Decimal.extractDecimal(buffer);
+        final double value = buffer.getDouble();
 
-        final TimedDecimal timedMoney = new TimedDecimal(time, percent);
+        final TimedDecimal timedMoney = new TimedDecimal(time, value);
         return timedMoney;
     }
 
@@ -32,7 +39,7 @@ public class TimedDecimalSerde implements Serde<TimedDecimal> {
     public byte[] toBytes(final TimedDecimal obj) {
         final ByteBuffer buffer = ByteBuffer.allocate(16);
         FDates.putFDate(buffer, obj.getTime());
-        Decimal.putDecimal(buffer, obj);
+        buffer.putDouble(obj.doubleValue());
         return buffer.array();
     }
 
