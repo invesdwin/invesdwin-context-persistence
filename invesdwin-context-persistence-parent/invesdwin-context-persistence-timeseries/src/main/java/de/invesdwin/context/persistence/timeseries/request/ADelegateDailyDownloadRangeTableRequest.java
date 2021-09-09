5439b4223c5398286b66359fe2763331d26cc62c
subes <gsubes@gmail.com>
2021-09-09 12:29:27 +0200

    leveldb request

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseries/src/main/java/de/invesdwin/context/persistence/timeseries/request/ADelegateDailyDownloadRangeTableRequest.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseries/src/main/java/de/invesdwin/context/persistence/timeseries/request/ADelegateDailyDownloadRangeTableRequest.java
@@ -12,12 +12,14 @@ import org.apache.commons.io.IOUtils;
 
 import de.invesdwin.context.integration.network.DailyDownloadCache;
 import de.invesdwin.context.persistence.timeseries.ezdb.ADelegateRangeTable;
+import de.invesdwin.context.persistence.timeseries.timeseriesdb.updater.ATimeSeriesUpdater;
 import de.invesdwin.util.collections.iterable.ICloseableIterator;
 import de.invesdwin.util.concurrent.reference.IReference;
 import de.invesdwin.util.error.Throwables;
 import de.invesdwin.util.marshallers.serde.ISerde;
 import de.invesdwin.util.marshallers.serde.basic.VoidSerde;
 import de.invesdwin.util.time.date.FDate;
+import ezdb.batch.Batch;
 
 @NotThreadSafe
 public abstract class ADelegateDailyDownloadRangeTableRequest<K, V>
@@ -54,19 +56,29 @@ public abstract class ADelegateDailyDownloadRangeTableRequest<K, V>
                             }
                         }, getNow());
                 final ICloseableIterator<V> reader = newReader(content);
-                try {
-                    while (true) {
-                        final V value = reader.next();
-                        final K key = extractKey(value);
-                        map.put(key, value);
+                try (Batch<K, V> batch = table.newBatch()) {
+                    int count = 0;
+                    try {
+                        while (true) {
+                            final V value = reader.next();
+                            final K key = extractKey(value);
+                            batch.put(key, value);
+                            count++;
+                            if (count >= ATimeSeriesUpdater.BATCH_FLUSH_INTERVAL) {
+                                batch.flush();
+                            }
+                        }
+                    } catch (final NoSuchElementException e) {
+                        //end reached
                     }
-                } catch (final NoSuchElementException e) {
-                    //end reached
+                    if (count > 0) {
+                        batch.flush();
+                    }
                 }
             }
         } catch (final Throwable t) {
             DailyDownloadCache.delete(getDownloadFileName());
-            map.deleteTable();
+            table.deleteTable();
             throw Throwables.propagate(t);
         }
     }
