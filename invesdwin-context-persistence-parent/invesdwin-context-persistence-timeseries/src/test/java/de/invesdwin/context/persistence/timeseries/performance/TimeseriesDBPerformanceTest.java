b212eb80634c3ced31ad9087d7f3324f4bd4f898
subes <gsubes@gmail.com>
2021-09-13 17:21:06 +0200

    progress

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseries/src/test/java/de/invesdwin/context/persistence/timeseries/performance/TimeseriesDBPerformanceTest.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseries/src/test/java/de/invesdwin/context/persistence/timeseries/performance/TimeseriesDBPerformanceTest.java
@@ -9,6 +9,8 @@ import javax.annotation.concurrent.NotThreadSafe;
 import org.junit.Test;
 
 import de.invesdwin.context.ContextProperties;
+import de.invesdwin.context.integration.streams.compressor.DisabledCompressionFactory;
+import de.invesdwin.context.integration.streams.compressor.ICompressionFactory;
 import de.invesdwin.context.persistence.timeseries.timeseriesdb.ATimeSeriesDB;
 import de.invesdwin.context.persistence.timeseries.timeseriesdb.IncompleteUpdateFoundException;
 import de.invesdwin.context.persistence.timeseries.timeseriesdb.updater.ATimeSeriesUpdater;
@@ -57,10 +59,10 @@ public class TimeseriesDBPerformanceTest extends ADatabasePerformanceTest {
                 return value;
             }
 
-            //            @Override
-            //            protected ICompressionFactory newCompressionFactory() {
-            //                return DisabledCompressionFactory.INSTANCE;
-            //            }
+            @Override
+            protected ICompressionFactory newCompressionFactory() {
+                return DisabledCompressionFactory.INSTANCE;
+            }
 
         };
 
