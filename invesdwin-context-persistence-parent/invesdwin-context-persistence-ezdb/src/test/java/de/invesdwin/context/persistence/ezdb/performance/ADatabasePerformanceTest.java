3c203ef3088cab7624859e05613a864372044d88
subes <gsubes@gmail.com>
2021-09-19 20:25:52 +0200

    size

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-ezdb/src/test/java/de/invesdwin/context/persistence/ezdb/performance/ADatabasePerformanceTest.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-ezdb/src/test/java/de/invesdwin/context/persistence/ezdb/performance/ADatabasePerformanceTest.java
@@ -18,7 +18,7 @@ import de.invesdwin.util.time.duration.Duration;
 public abstract class ADatabasePerformanceTest extends ATest {
 
     protected static final int READS = 100;
-    protected static final int VALUES = 100_000;
+    protected static final int VALUES = 10_000_000;
     protected static final String HASH_KEY = "HASH_KEY";
     protected static final int FLUSH_INTERVAL = ADelegateRangeTable.BATCH_FLUSH_INTERVAL;
 
