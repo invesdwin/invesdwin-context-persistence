d06f0f66d1185ff69b384543334ad6c99790da25
subes <gsubes@gmail.com>
2021-09-23 01:40:05 +0200

    recover

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-chronicle/src/main/java/de/invesdwin/context/persistence/chronicle/PersistentChronicleMapFactory.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-chronicle/src/main/java/de/invesdwin/context/persistence/chronicle/PersistentChronicleMapFactory.java
@@ -46,7 +46,7 @@ public class PersistentChronicleMapFactory<K, V> implements IPersistentMapFactor
             final File file = config.getFile();
             try {
                 Files.forceMkdirParent(file);
-                return mapBuilder.createPersistedTo(file);
+                return mapBuilder.createOrRecoverPersistedTo(file);
             } catch (final IOException e) {
                 throw new RuntimeException(e);
             }
