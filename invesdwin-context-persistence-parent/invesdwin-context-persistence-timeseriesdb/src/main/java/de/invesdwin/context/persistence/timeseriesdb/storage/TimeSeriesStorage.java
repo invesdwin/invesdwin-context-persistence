3e4feb9fef2449043a707e57e344ede41b4eab3d
subes <gsubes@gmail.com>
2021-09-23 19:50:25 +0200

    restore range table to test

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseriesdb/src/main/java/de/invesdwin/context/persistence/timeseriesdb/storage/TimeSeriesStorage.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseriesdb/src/main/java/de/invesdwin/context/persistence/timeseriesdb/storage/TimeSeriesStorage.java
@@ -5,17 +5,11 @@ import java.util.function.Supplier;
 
 import javax.annotation.concurrent.ThreadSafe;
 
-import de.invesdwin.context.integration.persistentmap.APersistentMap;
-import de.invesdwin.context.integration.persistentmap.IPersistentMapFactory;
 import de.invesdwin.context.integration.streams.compressor.ICompressionFactory;
-import de.invesdwin.context.integration.streams.compressor.lz4.FastLZ4CompressionFactory;
 import de.invesdwin.context.persistence.ezdb.ADelegateRangeTable;
 import de.invesdwin.context.persistence.ezdb.RangeTablePersistenceMode;
-import de.invesdwin.context.persistence.timeseriesdb.TimeseriesProperties;
-import de.invesdwin.context.persistence.timeseriesdb.storage.key.HashRangeKey;
-import de.invesdwin.context.persistence.timeseriesdb.storage.key.HashRangeKeySerde;
-import de.invesdwin.context.persistence.timeseriesdb.storage.key.HashRangeShiftUnitsKey;
-import de.invesdwin.context.persistence.timeseriesdb.storage.key.HashRangeShiftUnitsKeySerde;
+import de.invesdwin.context.persistence.timeseriesdb.storage.key.RangeShiftUnitsKey;
+import de.invesdwin.context.persistence.timeseriesdb.storage.key.RangeShiftUnitsKeySerde;
 import de.invesdwin.util.marshallers.serde.ISerde;
 import de.invesdwin.util.time.date.FDate;
 
@@ -25,9 +19,9 @@ public class TimeSeriesStorage {
     private final File directory;
     private final ICompressionFactory compressionFactory;
     private final ADelegateRangeTable<String, FDate, ChunkValue> fileLookupTable;
-    private final APersistentMap<HashRangeKey, SingleValue> latestValueLookupTable;
-    private final APersistentMap<HashRangeShiftUnitsKey, SingleValue> previousValueLookupTable;
-    private final APersistentMap<HashRangeShiftUnitsKey, SingleValue> nextValueLookupTable;
+    private final ADelegateRangeTable<String, FDate, SingleValue> latestValueLookupTable;
+    private final ADelegateRangeTable<String, RangeShiftUnitsKey, SingleValue> previousValueLookupTable;
+    private final ADelegateRangeTable<String, RangeShiftUnitsKey, SingleValue> nextValueLookupTable;
 
     public TimeSeriesStorage(final File directory, final Integer valueFixedLength,
             final ICompressionFactory compressionFactory) {
@@ -61,87 +55,68 @@ public class TimeSeriesStorage {
             }
 
         };
-        this.latestValueLookupTable = new APersistentMap<HashRangeKey, SingleValue>("latestValueLookupTable") {
+        this.latestValueLookupTable = new ADelegateRangeTable<String, FDate, SingleValue>("latestValueLookupTable") {
 
             @Override
-            public File getDirectory() {
+            protected File getDirectory() {
                 return directory;
             }
 
             @Override
-            public ISerde<SingleValue> newValueSerde() {
-                return FastLZ4CompressionFactory.INSTANCE.maybeWrap(SingleValueSerde.GET);
+            protected ISerde<SingleValue> newValueSerde() {
+                return SingleValueSerde.GET;
             }
 
             @Override
-            public ISerde<HashRangeKey> newKeySerde() {
-                return HashRangeKeySerde.GET;
-            }
-
-            //            @Override
-            //            protected void onDeleteTableFinished() {
-            //                throw new CorruptedTimeSeriesStorageException(getName());
-            //            }
-
-            @Override
-            protected IPersistentMapFactory<HashRangeKey, SingleValue> newFactory() {
-                return TimeseriesProperties.newPersistentMapFactory(false);
+            protected void onDeleteTableFinished() {
+                throw new CorruptedTimeSeriesStorageException(getName());
             }
 
         };
-        this.nextValueLookupTable = new APersistentMap<HashRangeShiftUnitsKey, SingleValue>("nextValueLookupTable") {
+        this.nextValueLookupTable = new ADelegateRangeTable<String, RangeShiftUnitsKey, SingleValue>(
+                "nextValueLookupTable") {
 
             @Override
-            public File getDirectory() {
+            protected File getDirectory() {
                 return directory;
             }
 
             @Override
-            public ISerde<HashRangeShiftUnitsKey> newKeySerde() {
-                return HashRangeShiftUnitsKeySerde.GET;
+            protected ISerde<RangeShiftUnitsKey> newRangeKeySerde() {
+                return RangeShiftUnitsKeySerde.GET;
             }
 
             @Override
-            public ISerde<SingleValue> newValueSerde() {
-                return FastLZ4CompressionFactory.INSTANCE.maybeWrap(SingleValueSerde.GET);
+            protected ISerde<SingleValue> newValueSerde() {
+                return SingleValueSerde.GET;
             }
 
-            //            @Override
-            //            protected void onDeleteTableFinished() {
-            //                throw new CorruptedTimeSeriesStorageException(getName());
-            //            }
-
             @Override
-            protected IPersistentMapFactory<HashRangeShiftUnitsKey, SingleValue> newFactory() {
-                return TimeseriesProperties.newPersistentMapFactory(false);
+            protected void onDeleteTableFinished() {
+                throw new CorruptedTimeSeriesStorageException(getName());
             }
         };
-        this.previousValueLookupTable = new APersistentMap<HashRangeShiftUnitsKey, SingleValue>(
+        this.previousValueLookupTable = new ADelegateRangeTable<String, RangeShiftUnitsKey, SingleValue>(
                 "previousValueLookupTable") {
 
             @Override
-            public File getDirectory() {
+            protected File getDirectory() {
                 return directory;
             }
 
             @Override
-            public ISerde<HashRangeShiftUnitsKey> newKeySerde() {
-                return HashRangeShiftUnitsKeySerde.GET;
+            protected ISerde<RangeShiftUnitsKey> newRangeKeySerde() {
+                return RangeShiftUnitsKeySerde.GET;
             }
 
             @Override
-            public ISerde<SingleValue> newValueSerde() {
-                return FastLZ4CompressionFactory.INSTANCE.maybeWrap(SingleValueSerde.GET);
+            protected ISerde<SingleValue> newValueSerde() {
+                return SingleValueSerde.GET;
             }
 
-            //            @Override
-            //            protected void onDeleteTableFinished() {
-            //                throw new CorruptedTimeSeriesStorageException(getName());
-            //            }
-
             @Override
-            protected IPersistentMapFactory<HashRangeShiftUnitsKey, SingleValue> newFactory() {
-                return TimeseriesProperties.newPersistentMapFactory(false);
+            protected void onDeleteTableFinished() {
+                throw new CorruptedTimeSeriesStorageException(getName());
             }
         };
     }
@@ -170,56 +145,46 @@ public class TimeSeriesStorage {
     }
 
     public void deleteRange_latestValueLookupTable(final String hashKey) {
-        latestValueLookupTable.removeAll((key) -> {
-            return hashKey.equals(key.getHashKey());
-        });
+        latestValueLookupTable.deleteRange(hashKey);
     }
 
     public void deleteRange_latestValueLookupTable(final String hashKey, final FDate above) {
         if (above == null) {
             deleteRange_latestValueLookupTable(hashKey);
         } else {
-            latestValueLookupTable.removeAll((key) -> {
-                return hashKey.equals(key.getHashKey()) && key.getRangeKey().isAfterOrEqualToNotNullSafe(above);
-            });
+            latestValueLookupTable.deleteRange(hashKey, above);
         }
     }
 
     public void deleteRange_nextValueLookupTable(final String hashKey) {
-        nextValueLookupTable.removeAll((key) -> {
-            return hashKey.equals(key.getHashKey());
-        });
+        nextValueLookupTable.deleteRange(hashKey);
     }
 
     public void deleteRange_previousValueLookupTable(final String hashKey) {
-        previousValueLookupTable.removeAll((key) -> {
-            return hashKey.equals(key.getHashKey());
-        });
+        previousValueLookupTable.deleteRange(hashKey);
     }
 
     public void deleteRange_previousValueLookupTable(final String hashKey, final FDate above) {
         if (above == null) {
             deleteRange_previousValueLookupTable(hashKey);
         } else {
-            previousValueLookupTable.removeAll((key) -> {
-                return hashKey.equals(key.getHashKey()) && key.getRangeKey().isAfterOrEqualToNotNullSafe(above);
-            });
+            previousValueLookupTable.deleteRange(hashKey, new RangeShiftUnitsKey(above, 0));
         }
     }
 
     public SingleValue getOrLoad_latestValueLookupTable(final String hashKey, final FDate key,
             final Supplier<SingleValue> loadable) {
-        return latestValueLookupTable.getOrLoad(new HashRangeKey(hashKey, key), loadable);
+        return latestValueLookupTable.getOrLoad(hashKey, key, loadable);
     }
 
     public SingleValue getOrLoad_nextValueLookupTable(final String hashKey, final FDate date,
             final int shiftForwardUnits, final Supplier<SingleValue> loadable) {
-        return nextValueLookupTable.getOrLoad(new HashRangeShiftUnitsKey(hashKey, date, shiftForwardUnits), loadable);
+        return nextValueLookupTable.getOrLoad(hashKey, new RangeShiftUnitsKey(date, shiftForwardUnits), loadable);
     }
 
     public SingleValue getOrLoad_previousValueLookupTable(final String hashKey, final FDate date,
             final int shiftBackUnits, final Supplier<SingleValue> loadable) {
-        return previousValueLookupTable.getOrLoad(new HashRangeShiftUnitsKey(hashKey, date, shiftBackUnits), loadable);
+        return previousValueLookupTable.getOrLoad(hashKey, new RangeShiftUnitsKey(date, shiftBackUnits), loadable);
     }
 
 }
