e419d169b0fc756bca8321843d24753ec8320771
Edwin Stang <gsubes@gmail.com>
2022-04-09 00:13:35 +0200

    parallel writes

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseriesdb/src/test/java/de/invesdwin/context/persistence/timeseriesdb/performance/TimeseriesDBPerformanceTest.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseriesdb/src/test/java/de/invesdwin/context/persistence/timeseriesdb/performance/TimeseriesDBPerformanceTest.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import de.invesdwin.context.ContextProperties;
@@ -26,6 +27,7 @@ import de.invesdwin.util.time.date.FDate;
 import de.invesdwin.util.time.duration.Duration;
 
 @NotThreadSafe
+@Disabled("manual test")
 public class TimeseriesDBPerformanceTest extends ADatabasePerformanceTest {
 
     @Test
