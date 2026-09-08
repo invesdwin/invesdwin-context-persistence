3cf0b74e749785365672aeed833b2add17a9c2b9
Edwin Stang <gsubes@gmail.com>
2026-09-08 16:24:04 +0200

    some progress with metadata

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseriesdb/src/main/java/de/invesdwin/context/persistence/timeseriesdb/updater/ATimeSeriesUpdater.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseriesdb/src/main/java/de/invesdwin/context/persistence/timeseriesdb/updater/ATimeSeriesUpdater.java
@@ -158,6 +158,7 @@ public abstract class ATimeSeriesUpdater<K, V> implements ITimeSeriesUpdater<K, 
     private void doUpdate() {
         //System.out.println("TODO: turn this into a transaction which collects the updated index and properties which both are saved at the end only");
         //TODO: we should also version the index files so that other updaters in other processes can switch to that correctly?
+        //TODO: maybe also make metadata update versioned or atomic via move?
         final PrepareForUpdateResult<V> prepareForUpdateResult = lookupTable.prepareForUpdate(shouldRedoLastFile());
         final FDate updateFrom = prepareForUpdateResult.getUpdateFrom();
         final List<V> lastValues = prepareForUpdateResult.getLastValues();
