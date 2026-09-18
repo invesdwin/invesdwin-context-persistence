6ad0dda0692f1ba50514b6d18a1b01a36a357372
Edwin Stang <gsubes@gmail.com>
2026-09-18 18:16:13 +0200

    handle concurrent table deletion property writes (first one wine)

--- invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseriesdb/src/main/java/de/invesdwin/context/persistence/timeseriesdb/storage/TimeSeriesStorage.java
+++ invesdwin-context-persistence-parent/invesdwin-context-persistence-timeseriesdb/src/main/java/de/invesdwin/context/persistence/timeseriesdb/storage/TimeSeriesStorage.java
@@ -1,6 +1,7 @@
 package de.invesdwin.context.persistence.timeseriesdb.storage;
 
 import java.io.File;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.NoSuchFileException;
 import java.util.function.Supplier;
 
@@ -143,11 +144,21 @@ public class TimeSeriesStorage {
 
     private void updateResetTableUnchecked(final APersistentMap<?, ?> table, final FDate lastResetShared,
             final FDate now) {
-        final String key = newResetTableKey(table);
-        if (lastResetShared == null) {
+        try {
+            final String key = newResetTableKey(table);
+            if (lastResetShared == null) {
+                directory.getStoragePropertiesShared().setDate(key, now);
+            }
             directory.getStoragePropertiesPerNode().setDate(key, now);
+        } catch (final Throwable t) {
+            /*
+             * ignore if another thread is also writing this property at the same time, likely it will be the same
+             * value, otherwise it is not critical if another table deletion happens
+             */
+            if (!Throwables.isCausedByType(t, FileAlreadyExistsException.class)) {
+                throw t;
+            }
         }
-        directory.getStoragePropertiesShared().setDate(key, now);
     }
 
     private void updateResetTable(final APersistentMap<?, ?> table) {
